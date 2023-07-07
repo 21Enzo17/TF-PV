15d89a8e302dabe9dee82c5b36da7620b037e564
johan <g@g-PC>
2023-07-07 00:44:23 -0300

    avances en el proyecto

---
+++ src/main/java/ar/edu/unju/fi/TPFINALPV/repository/PesoIdealRepository.java
@@ -0,0 +1,16 @@
+package ar.edu.unju.fi.TPFINALPV.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import ar.edu.unju.fi.TPFINALPV.entity.PesoIdeal;
+
+
+@Repository
+public class PesoIdealRepository extends CrudRepository <PesoIdeal,Long> {
+
+    public List<PesoIdeal> findByEstado(boolean estado);
+    
+}
