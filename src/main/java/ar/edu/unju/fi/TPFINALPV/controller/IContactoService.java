6d227a27546473e8760dd8c8fe0a23546e5520f9
Ezequiel <ezequielmamani7>
2023-07-09 19:15:52 -0300

    Se diseño la pagina de contactos, en conjunto con su respectivo formulario para la comunicación entre usuario y cliente

---
+++ src/main/java/ar/edu/unju/fi/TPFINALPV/controller/IContactoService.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.TPFINALPV.controller;
+import ar.edu.unju.fi.TPFINALPV.entity.Contacto;
+import java.util.List;
+public interface IContactoService {
+	
+	void crearContacto(Contacto contacto);
+
+	void eliminarContacto(Long id);
+	
+	public List<Contacto> getListaDeContactos();
+	
+}
